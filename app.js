648496ce71f725eb1d8db53d47fe148513836a74
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-03-06 00:33:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,100 +1,100 @@
-// var product = prompt ("Enter Product");
+var product = prompt ("Enter Product");
 
-// if (product === "Biryani"){
-//     alert("Available hai \n Your order is access");
-// }
-// else if(product === "biryani") {
-//     alert("Biryani is available but order book krwane k liye (B) capital likhen \n (Shukriya)")
-// }
-// else {
-//     alert("Sorry only Biryani is available");
-// }
-
-
-// var age = +prompt ("Enter your age");
-
-// if (age <= 17){
-//     alert("Ticket price is Rs.30");
-// }
-// else if (age >= 18 && age < 50){
-//     alert("Ticket price is Rs.50");
-// }
-// else {
-//     alert("Ticket is free");
-// }
+if (product === "Biryani"){
+    alert("Available hai \n Your order is access");
+}
+else if(product === "biryani") {
+    alert("Biryani is available but order book krwane k liye (B) capital likhen \n (Shukriya)")
+}
+else {
+    alert("Sorry only Biryani is available");
+}
 
 
-// var x = "Enter value";
-// var y = "Your value is ";
-// var a = +prompt (x,y);
-// var b = a + 10;
-// document.write("The output is" + " " + b)
+var age = +prompt ("Enter your age");
+
+if (age <= 17){
+    alert("Ticket price is Rs.30");
+}
+else if (age >= 18 && age < 50){
+    alert("Ticket price is Rs.50");
+}
+else {
+    alert("Ticket is free");
+}
 
 
-// var age = +prompt ("Enter your age");
-// var Gender = prompt ("Enter your Gender");
-
-// if (age > 18 || Gender == "male"){
-//     alert("You are allowed to ride")
-// }else{
-//     alert("You are not allowed")
-// }
-
-// var weight = +prompt ("Enter your weight", "your weight is ");
-// var time = prompt ("Enter your time", "your time is ");
-// var $age = prompt ("Enter your age", "your age is ");
-// var $gender = prompt ("Enter your gender", "your gender is ");
+var x = "Enter value";
+var y = "Your value is ";
+var a = +prompt (x,y);
+var b = a + 10;
+document.write("The output is" + " " + b)
 
 
-// if (weight > 300 && time < 6 && $age > 17 && $gender === "male") {
-//     alert("Come to our tryout");
-// }else{
-//     alert("Come to our cookout!");
-// }
+var age = +prompt ("Enter your age");
+var Gender = prompt ("Enter your Gender");
+
+if (age > 18 || Gender == "male"){
+    alert("You are allowed to ride")
+}else{
+    alert("You are not allowed")
+}
+
+var weight = +prompt ("Enter your weight", "your weight is ");
+var time = prompt ("Enter your time", "your time is ");
+var $age = prompt ("Enter your age", "your age is ");
+var $gender = prompt ("Enter your gender", "your gender is ");
 
 
-// Percentage
-
-// var percentage = +prompt ("Enter your percentage");
-// if (percentage >= 80 && percentage <= 100){
-//     alert("A+");
-// }else if (percentage >= 70 && percentage <= 80){
-//     alert("A");
-// }else if (percentage >= 60 && percentage <= 70){
-//     alert("B");
-// }else if (percentage >= 50 && percentage <= 50){
-//     alert("c");
-// }else if (percentage >= 40 && percentage <= 50){
-//     alert("D");
-// }else if (percentage >= 33 && percentage <= 40){
-//     alert("E");
-// }else if (percentage >= 0 && percentage <= 33){
-//     alert("F");
-// }else {
-//     alert("You did not write correct percentage")
-// }
+if (weight > 300 && time < 6 && $age > 17 && $gender === "male") {
+    alert("Come to our tryout");
+}else{
+    alert("Come to our cookout!");
+}
 
 
-// var city = prompt("Enter City")
-// if (city === "karachi"){
-//     alert("Welcome to city of lights")
-// }else if (city === "Lahore"){
-//     alert("Welcome to city of lights")
-// }else if (city === "Islamabad"){
-//     alert("Welcome to city of lights")
-// }else if (city === "Hyderabad"){
-//     alert("Welcome to city of lights")
-// }else {
-//     alert("Enter city only")
-// }
+Percentage
 
-// var _gender = prompt("Enter your Gender")
-// if(_gender === "male"){
-//  alert("Good morning Sir")
-// }else if(_gender === "female"){
-//     alert("Good morning Ma'am")
-// }else{
-//     alert("Enter only Gender")
-// }
+var percentage = +prompt ("Enter your percentage");
+if (percentage >= 80 && percentage <= 100){
+    alert("A+");
+}else if (percentage >= 70 && percentage <= 80){
+    alert("A");
+}else if (percentage >= 60 && percentage <= 70){
+    alert("B");
+}else if (percentage >= 50 && percentage <= 50){
+    alert("c");
+}else if (percentage >= 40 && percentage <= 50){
+    alert("D");
+}else if (percentage >= 33 && percentage <= 40){
+    alert("E");
+}else if (percentage >= 0 && percentage <= 33){
+    alert("F");
+}else {
+    alert("You did not write correct percentage")
+}
+
+
+var city = prompt("Enter City")
+if (city === "karachi"){
+    alert("Welcome to city of lights")
+}else if (city === "Lahore"){
+    alert("Welcome to city of lights")
+}else if (city === "Islamabad"){
+    alert("Welcome to city of lights")
+}else if (city === "Hyderabad"){
+    alert("Welcome to city of lights")
+}else {
+    alert("Enter city only")
+}
+
+var _gender = prompt("Enter your Gender")
+if(_gender === "male"){
+ alert("Good morning Sir")
+}else if(_gender === "female"){
+    alert("Good morning Ma'am")
+}else{
+    alert("Enter only Gender")
+}
 
 
